a6d7213783645bd322def8f59bbcc12cacd9c65c
danylagacione <57449365+danylagacione@users.noreply.github.com>
2019-11-19 21:14:50 -0300

    update exercício calculo_imposto

--- Aula9- Web/aula9-exercicioimposto.py
+++ Aula9- Web/aula9-exercicioimposto.py
@@ -18,3 +18,46 @@
 #---         de 4664,69 ate ------- - percetual = 27,5%
 #--- Base - http://www.calculador.com.br/calculo/salario-liquido
 
+
+print('='*50, '\n')
+
+# nome_completo = input('Digite seu nome completo:')
+# cpf = input('Digite seu cpf:')
+# num_registro = int( input('Digite seu registro:') )
+# cargo = input('Digite seu cargo:')
+
+salario = float( input('Digite seu salario:') )
+
+inss = calculo_inss(salario)
+irrf = calculo_irrf(salario, inss)
+
+salario_liquido = salario - inss - irrf
+print(f'Inss: {inss}')
+print(f'Irrf: {irrf}')
+print(f'Seu salário liquido é {salario_liquido}')
+
+ if(salario>0 and salario <= 1751.81):
+        inss = salario * 0.08
+    elif (salario >1751.81 and salario<=2919.72 ):
+        inss = salario * 0.090
+    elif (salario >2919.72 and salario<=5839.45 ):
+        inss = salario * 0.11
+    else:
+        inss = 642.34
+        
+irrf = 0
+    if(salario>1903.98 and salario <= 2826.65):
+        irrf = (((salario - inss) * 0.075)-142.80)
+    elif (salario >=2826.66 and salario<=3751.05 ):
+        irrf = (((salario - inss) * 0.15)-354.80)
+    elif (salario >=3751.06 and salario<=4664.68 ):
+        irrf = (((salario - inss) * 0.225)-636.13)
+    elif(salario > 4664.68):
+        irrf = (((salario - inss) * 0.275)-869.36)
+        
+#-- os 2 blocos de códigos (if, elif e else) podem ser colocados em método (def + o que quer chamar + nome para o metodo).
+
+print( '\n'*2,'='*50)
+
+
+
