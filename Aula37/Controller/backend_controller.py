e3136fca4cab590eeb63b902e464afafa1a3efd6
danylagacione <danielycrys24@gmail.com>
2020-01-28 09:29:42 -0300

    att

--- Aula37/Controller/backend_controller.py
+++ Aula37/Controller/backend_controller.py
@@ -7,24 +7,25 @@ from Model.model_backend import BackEnd
 
 class BackEndController:
     dao = BackEndDao()
-    model = BackEnd()
-    
+        
     def listar_backend(self):
-        lista_backend = []
-        lista_tuplas = self.dao.listar_backend()
-        for s in lista_tuplas:
-            squad = Squad()
-            squad.id = s[0]
-            squad.nome = s[1]
-            lista_backend.append(squad)
-        return lista_backend
+        # lista_backend = []
+        # lista_tuplas = self.dao.listar_backend()
+        # for s in lista_tuplas:
+        #     squad = Squad()
+        #     squad.id = s[0]
+        #     squad.nome = s[1]
+        #     lista_backend.append(squad)
+        # return lista_backend
+        return self.dao.listar_backend()
 
     def buscar_id_backend(self, id):
-        s = self.dao.buscar_por_id(id)
-        squad = Squad()
-        squad.id = s[0]
-        squad.nome = s[1]
-        return squad
+        # s = self.dao.buscar_por_id(id)
+        # squad = Squad()
+        # squad.id = s[0]
+        # squad.nome = s[1]
+        # return squad
+        return self.dao.buscar_id_backend
 
     def salvar_backend(self, squad:Squad):
         return self.dao.salvar(squad)
@@ -34,3 +35,18 @@ class BackEndController:
 
     def deletar_backend(self, id):
         self.dao.deletar(id)
+
+#  def listar_todos(self):
+#         return self.dao.listar_todos()
+
+#     def buscar_por_id(self, id):
+#         return self.dao.buscar_por_id(id)
+
+#     def salvar(self, linguagemBackend:LinguagemBackend):
+#         return self.dao.salvar(linguagemBackend)
+
+#     def alterar(self, linguagemBackend:LinguagemBackend):
+#         self.dao.alterar(linguagemBackend)
+
+#     def deletar(self, id):
+#         self.dao.deletar(id)        
