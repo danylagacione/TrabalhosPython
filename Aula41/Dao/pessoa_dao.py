046325940aa8c773c67369d1d684544e29e89735
danylagacione <danielycrys24@gmail.com>
2020-01-30 11:53:14 -0300

    Aula 41 att

---
+++ Aula41/Dao/pessoa_dao.py
@@ -0,0 +1,10 @@
+
+class PessoaDao:
+    def list_all(self):
+        return 'Listando todos os dados da tabela'
+
+    def get_by_id(self, id):
+        return 'Listando os dados do id: {}'.format (id)
+
+    def post_insert(self):
+        return ''
