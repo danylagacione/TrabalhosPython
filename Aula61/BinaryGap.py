ffb98896b846b135cc18710f968a11c285094df1
danylagacione <danielycrys24@gmail.com>
2020-02-27 11:56:04 -0300

    exercicio Binary

--- Aula61/BinaryGap.py
+++ Aula61/BinaryGap.py
@@ -21,3 +21,18 @@
 # Dado N = 32, a função deve retornar 0, porque N tem representação binária '100000'
 # e, portanto, sem lacunas binárias.
 
+def binary(numero):
+    #numero = int(input('Número:'))
+    numero = (bin(numero))
+    numero = (numero[2:]).strip('').split('1')
+    #numero = len(max(numero))
+    print(numero)
+
+# melhorar o código
+print(str(binary(9)))
+
+# def solution(N):
+#   return len(max(bin(N)[2:].strip('0').strip('1').split('1')))
+#
+# print(solution(200))
+
